f3973afbe3d3cd4b52454efc31f6f7cefe63f733
dev-kalam <abulkalamripon09@gmail.com>
2025-07-06 21:56:51 +0600

    Create VoiceInput.java

---
+++ VoiceInput.java
@@ -0,0 +1,11 @@
+
+package testassistant;
+
+import javax.swing.JOptionPane;
+
+public class VoiceInput {
+    // For simulation - replace with actual voice recognition
+    public String listen() {
+        return JOptionPane.showInputDialog("Speak your command (simulated voice input):");
+    }
+}
